fd38ee35ab31d500af618ed1fb9bb8ba5c2d6c43
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-05 13:07:57 +0530

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
@@ -11,6 +12,7 @@ const AppLayout = () => {
     <>
       <Header />
       <Body />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2023 FoodHunger. All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
