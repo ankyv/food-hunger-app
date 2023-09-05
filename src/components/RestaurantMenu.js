d0a03e82d6a34aade98c0e87135fa1ad95bb422b
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-05 17:55:16 +0530

    Added menu list in RestaurantMenu component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -9,6 +9,9 @@ const RestaurantMenu = () => {
 
   const restaurantInfo = restaurant?.cards[0]?.card?.card?.info;
 
+  const restaurantMenu =
+    restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
   useEffect(() => {
     getRestaurantInfo(resId);
   }, []);
@@ -43,6 +46,36 @@ const RestaurantMenu = () => {
           <h4>{restaurantInfo?.avgRating} stars</h4>
           <h4>{restaurantInfo?.costForTwo}</h4>
         </div>
+      </div>
+      <div className="restaurant-menu">
+        {restaurantMenu?.map((item) => {
+          return item?.card?.card?.itemCards ? (
+            <div key={item?.card?.card?.title} className="menu-category">
+              <h1>{item?.card?.card?.title}</h1>
+              <div className="menu-items">
+                {item?.card?.card?.itemCards?.map((foodItem) => {
+                  return (
+                    <div key={foodItem?.card?.info?.id} className="food-item">
+                      <div className="food-item-details">
+                        <h2>{foodItem?.card?.info?.name}</h2>
+                        <h4>Rs. {foodItem?.card?.info?.price / 100}</h4>
+                        <p>{foodItem?.card?.info?.description}</p>
+                      </div>
+                      <div className="food-item-image">
+                        <img
+                          src={
+                            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" +
+                            foodItem?.card?.info?.imageId
+                          }
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : null;
+        })}
       </div>
     </div>
   );
