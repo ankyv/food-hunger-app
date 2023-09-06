14c1f708c662d17a728e4c00ffefe5ba2cd4b0fa
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-07 00:28:00 +0530

    Updated FoodItem component

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,6 +1,10 @@
 import { IMG_CDN_URL } from "../constants";
+import { useDispatch } from "react-redux";
+import { increaseItem, decreaseItem, removeItem } from "../utils/cartSlice";
 
 const FoodItem = ({ id, count, name, imageId, price }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="food-item">
       <div className="food-image">
@@ -11,9 +15,25 @@ const FoodItem = ({ id, count, name, imageId, price }) => {
         <h4>Rs. {price / 100}</h4>
       </div>
       <div className="food-quantity">
-        <button>-</button>
+        <button
+          onClick={() => {
+            if (count === 1) {
+              dispatch(removeItem(id));
+            } else {
+              dispatch(decreaseItem(id));
+            }
+          }}
+        >
+          -
+        </button>
         <p>{count}</p>
-        <button>+</button>
+        <button
+          onClick={() => {
+            dispatch(increaseItem(id));
+          }}
+        >
+          +
+        </button>
       </div>
     </div>
   );
