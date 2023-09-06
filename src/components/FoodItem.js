90be0d2237efc0c1f08cb140c82fcc2f4506e3ec
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-06 18:05:41 +0530

    Added quantity functionality inside FoodItem

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,6 +1,19 @@
 import { IMG_CDN_URL } from "../constants";
+import { useState } from "react";
 
 const FoodItem = ({ name, imageId, price }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncreaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
   return (
     <div className="food-item">
       <div className="food-image">
@@ -9,6 +22,23 @@ const FoodItem = ({ name, imageId, price }) => {
       <div className="food-details">
         <h2>{name}</h2>
         <h4>Rs. {price / 100}</h4>
+      </div>
+      <div className="food-quantity">
+        <button
+          onClick={() => {
+            handleDecreaseQuantity();
+          }}
+        >
+          -
+        </button>
+        <p>{quantity}</p>
+        <button
+          onClick={() => {
+            handleIncreaseQuantity();
+          }}
+        >
+          +
+        </button>
       </div>
     </div>
   );
