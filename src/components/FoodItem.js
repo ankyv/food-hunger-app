873a45b24887dc474fd799b1cf3fd5ee92f17ef6
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-07 00:43:56 +0530

    Added price functionality in FoodItem

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,9 +1,26 @@
 import { IMG_CDN_URL } from "../constants";
 import { useDispatch } from "react-redux";
 import { increaseItem, decreaseItem, removeItem } from "../utils/cartSlice";
+import { useState } from "react";
 
 const FoodItem = ({ id, count, name, imageId, price }) => {
+  const [totalPrice, setTotalPrice] = useState(count * price);
+
   const dispatch = useDispatch();
+
+  const handleDecreaseItem = () => {
+    if (count === 1) {
+      dispatch(removeItem(id));
+    } else {
+      dispatch(decreaseItem(id));
+      setTotalPrice(totalPrice - price);
+    }
+  };
+
+  const handleIncreaseItem = () => {
+    dispatch(increaseItem(id));
+    setTotalPrice(totalPrice + price);
+  };
 
   return (
     <div className="food-item">
@@ -13,27 +30,12 @@ const FoodItem = ({ id, count, name, imageId, price }) => {
       <div className="food-details">
         <h2>{name}</h2>
         <h4>Rs. {price / 100}</h4>
+        <h3>Total Price: {totalPrice / 100}</h3>
       </div>
       <div className="food-quantity">
-        <button
-          onClick={() => {
-            if (count === 1) {
-              dispatch(removeItem(id));
-            } else {
-              dispatch(decreaseItem(id));
-            }
-          }}
-        >
-          -
-        </button>
+        <button onClick={() => handleDecreaseItem()}>-</button>
         <p>{count}</p>
-        <button
-          onClick={() => {
-            dispatch(increaseItem(id));
-          }}
-        >
-          +
-        </button>
+        <button onClick={() => handleIncreaseItem()}>+</button>
       </div>
     </div>
   );
