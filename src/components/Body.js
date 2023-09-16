644506ca73568d274532c8322ed57b79d6c6d58a
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-16 22:24:18 +0530

    Fixed APIs

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import { filterData } from "../utils/helper";
-import { API_DATA_URL } from "../constants";
+import { API_DESKTOP_URL, API_MOBILE_URL } from "../constants";
 import useOnline from "../utils/useOnline";
 
 const Body = () => {
@@ -17,11 +17,15 @@ const Body = () => {
   const isOnline = useOnline();
 
   useEffect(() => {
-    getRestaurants();
+    if (window.screen.width < 1000) {
+      getRestaurants(API_MOBILE_URL);
+    } else {
+      getRestaurants(API_DESKTOP_URL);
+    }
   }, []);
 
-  async function getRestaurants() {
-    const response = await fetch(API_DATA_URL);
+  async function getRestaurants(API_TO_CALL) {
+    const response = await fetch(API_TO_CALL);
     const json = await response.json();
     // console.log(
     //   json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
